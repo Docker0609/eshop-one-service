3481dc104ce6778c4b9c03140a933070703ab1b3
kitty <hello@.com>
2019-08-28 01:18:28 +0800

    库存服务与价格服务的代理接口开发

--- src/main/java/com/roncoo/eshop/one/service/EshopPriceService.java
+++ src/main/java/com/roncoo/eshop/one/service/EshopPriceService.java
@@ -4,11 +4,16 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @FeignClient(value = "eshop-price-service")
 public interface EshopPriceService {
 
     @RequestMapping(value = "/product-price/findByProductId",method = RequestMethod.GET)
     String findByProductId(@RequestParam(value = "productId") Long productId);
+
+    
+    
+    
 
 }
