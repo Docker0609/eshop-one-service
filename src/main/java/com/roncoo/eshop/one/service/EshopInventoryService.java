e2176d42028189784b60703c2cbc7992d9ac66e8
kitty <hello@.com>
2019-08-28 01:44:58 +0800

    基于hystrix进行接口统一降级

--- src/main/java/com/roncoo/eshop/one/service/EshopInventoryService.java
+++ src/main/java/com/roncoo/eshop/one/service/EshopInventoryService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient(value = "eshop-inventory-service")
+@FeignClient(value = "eshop-inventory-service" )
 public interface EshopInventoryService {
 
 	 @RequestMapping(value = "/product-inventory/findByProductId",method = RequestMethod.GET)
--- src/main/java/com/roncoo/eshop/one/service/EshopPriceService.java
+++ src/main/java/com/roncoo/eshop/one/service/EshopPriceService.java
@@ -4,16 +4,13 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@FeignClient(value = "eshop-price-service")
+import com.roncoo.eshop.one.service.fallback.EshopPriceServiceFallback;
+
+@FeignClient(value = "eshop-price-service", fallback = EshopPriceServiceFallback.class)
 public interface EshopPriceService {
 
     @RequestMapping(value = "/product-price/findByProductId",method = RequestMethod.GET)
     String findByProductId(@RequestParam(value = "productId") Long productId);
-
-    
-    
-    
 
 }
