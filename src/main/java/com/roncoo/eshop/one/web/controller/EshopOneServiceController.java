f0a834a65dcdecf777446e6a5d9d04970cac8e4c
kitty <hello@.com>
2019-08-28 01:11:33 +0800

    基于Spring Cloud构建OneService服务\代码

---
+++ src/main/java/com/roncoo/eshop/one/web/controller/EshopOneServiceController.java
@@ -0,0 +1,13 @@
+package com.roncoo.eshop.one.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/one")
+public class EshopOneServiceController {
+
+	
+	
+}
